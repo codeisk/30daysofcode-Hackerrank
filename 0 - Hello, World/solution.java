06212bd0818a74bd7c8cdb4a935d204049279907
Irfan Shaikh <irfansk.se@gmail.com>
2021-11-07 13:13:37 +0530

    Java Solution - Day 1

---
+++ 0 - Hello, World/solution.java
@@ -0,0 +1,17 @@
+/**
+Author: Irfan Shaikh
+
+// Problem: https://www.hackerrank.com/challenges/30-hello-world/problem
+*/
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        
+        System.out.println(s);
+    }
+}
